38b0fd8aee8157eed31c4b0a60ff8641721b82f9
Dor-B <dor46200@gmail.com>
2020-10-17 19:53:24 +0300

    Add admin apps script url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const GDRIVE_FILE_URL_PREFIX = 'https://drive.google.com/file/d/'
 export const UPLOAD_FILE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzhkE8gwCcmUeZiHW_D2ad2A5xR7XVEnzttyANgoootjE2cieRy/exec"
-export const ADMIN_APPS_SCRIPT_URL = ''
+export const ADMIN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqozd6ENmmycCZJAIhG1CD99kKajdMpVjC40vl0M2VKkYipMZj/exec'
