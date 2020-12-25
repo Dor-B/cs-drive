bf12b78c6885737db82e6c5baca6faebc7fa84f4
Dor-B <dor46200@gmail.com>
2020-12-25 17:27:45 +0200

    Add analytics

--- src/db.js
+++ src/db.js
@@ -2,19 +2,21 @@
 
 import firebase from 'firebase/app'
 import 'firebase/database'
+import 'firebase/analytics'
 
-export const db = firebase
-    .initializeApp({
-        apiKey: "AIzaSyCguQreALBDnfJgWrf1OLyWa-PJjePlv2g",
-        authDomain: "technion-cs-drive.firebaseapp.com",
-        databaseURL: "https://technion-cs-drive.firebaseio.com",
-        projectId: "technion-cs-drive",
-        storageBucket: "technion-cs-drive.appspot.com",
-        messagingSenderId: "1082094948369",
-        appId: "1:1082094948369:web:4697c08eb0ec9b471fa1b0",
-        measurementId: "G-2BREK6ESM2"
-      })
-    .database()
+const fb = firebase
+.initializeApp({
+    apiKey: "AIzaSyCguQreALBDnfJgWrf1OLyWa-PJjePlv2g",
+    authDomain: "technion-cs-drive.firebaseapp.com",
+    databaseURL: "https://technion-cs-drive.firebaseio.com",
+    projectId: "technion-cs-drive",
+    storageBucket: "technion-cs-drive.appspot.com",
+    messagingSenderId: "1082094948369",
+    appId: "1:1082094948369:web:4697c08eb0ec9b471fa1b0",
+    measurementId: "G-2BREK6ESM2"
+  })
+export const db = fb.database()
+export const analytics = firebase.analytics()
 
 
 // Export types that exists in Firestore - Uncomment if you need them in your app
