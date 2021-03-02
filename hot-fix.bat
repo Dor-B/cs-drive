47a1c38a906d729db15e3883192c877890fd7dfe
Dor-B <dor46200@gmail.com>
2021-03-02 15:43:09 +0200

    Add hot-fix delploy batch script

---
+++ hot-fix.bat
@@ -0,0 +1,13 @@
+ECHO OFF
+git branch | find "* master" > NUL & IF ERRORLEVEL 1 (
+    ECHO Go to master branch first!
+) ELSE (
+    git add -A
+    git commit -m %1
+    git push
+    git checkout stable
+    git merge master
+    git push
+    npm run build
+    firebase deploy --only hosting -m %1
+)
