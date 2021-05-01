faa55a4801b44a781b07cfd00f9e9f062b69dccb
Dor-B <dor46200@gmail.com>
2021-05-01 21:47:08 +0300

    Add tutorial video for uploading videos under information dialog

--- src/components/TutorialDialog.vue
+++ src/components/TutorialDialog.vue
@@ -16,14 +16,46 @@
 
       <v-card>
         <v-card-title>איך משתמשים בדרייב</v-card-title>
-        <v-card-text>
-          <video autoplay muted loop width="850px" id="videoElement">
-            <source v-if="dialog" src="../assets/tutorialVideo.mp4" type="video/mp4">
-            Your browser does not support the video tag.
-          </video>        
-        </v-card-text>
+        <v-tabs
+          hide-slider
+          v-model="tab"
+        >
+          <v-tab>
+            חיפוש קבצים
+          </v-tab>
+          <v-tab>
+            העלאת חומרים
+          </v-tab>
 
-        <v-card-actions>
+          <v-tab-item>
+            <v-card flat>
+              <v-card-text>
+                <video autoplay muted loop width="830px" id="findFilesVideo">
+                  <source v-if="dialog" src="../assets/tutorialVideo.mp4" type="video/mp4">
+                  Your browser does not support the video tag.
+                </video>        
+              </v-card-text>
+            </v-card>
+
+          </v-tab-item>
+          <v-tab-item>
+            <v-card flat>
+              <v-card-text>
+                <video autoplay muted loop width="830px" id="uploadFilesVideo">
+                  <source v-if="dialog" src="../assets/uploadFileVideo.mp4" type="video/mp4">
+                  Your browser does not support the video tag.
+                </video>
+                <br>
+                <v-divider></v-divider>
+                <ul class="font-weight-bold" id="uploadInfoText">
+                  <li>חשוב להוסיף שם מסכם או תיוגים אחרים כדי להבדיל משאר הקבצים מאותו הסוג</li>
+                  <li>קבצים יופיעו באתר רק לאחר אישור אדמיניסטרטור</li>
+                </ul>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+        <v-card-actions style="position:sticky;bottom:5px;">
           <v-spacer></v-spacer>
           <v-btn
             color="primary"
@@ -52,25 +84,32 @@
     data() {
         return {
             dialog: this.defaultVisibillity,
-            videoURL: "../assets/tutorialVideo.mp4",
+            videoIds: ["findFilesVideo", "uploadFilesVideo"],
+            tab: 0,
         }
     },
     computed: {
         
     },
     methods: {
-        
-    },
-    watch: {
-      dialog: function(newDialogVal){
-        if(newDialogVal){
-          let video = document.getElementById("videoElement")
+        replayVideo(videoId){
+          let video = document.getElementById(videoId)
           if(video){
             video.pause()
             video.currentTime = 0
             video.play()
           }
         }
+    },
+    watch: {
+      dialog: function(newDialogVal){
+        if(newDialogVal){
+          this.replayVideo(this.videoIds[this.tab])
+        }
+      },
+      tab: function(newTab){
+        let videoId = this.videoIds[newTab]
+        this.replayVideo(videoId)
       }
     },
     created: function(){
@@ -78,3 +117,11 @@
     }
   }
 </script>
+
+<style scoped>
+#uploadInfoText{
+  padding-right: 15px;
+  padding-left: 15px;
+  font-size: medium;
+}
+</style>
