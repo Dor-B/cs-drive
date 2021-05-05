5c17d736e741836fcc13f296a0d2cde19876eb19
Dor-B <dor46200@gmail.com>
2021-05-05 19:36:35 +0300

    Fix delete files which were uploaded by another user

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const GDRIVE_FILE_URL_PREFIX = 'https://drive.google.com/file/d/'
 export const UPLOAD_FILE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzhkE8gwCcmUeZiHW_D2ad2A5xR7XVEnzttyANgoootjE2cieRy/exec"
-export const ADMIN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqozd6ENmmycCZJAIhG1CD99kKajdMpVjC40vl0M2VKkYipMZj/exec'
-export const ADMIN_CHANGE_FILE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzEsoon3s1FCeiJFu6t62bAl4MnnBHcwUVmABY1Tsgdd3UAZSs/exec'
+export const ADMIN_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxpFEBvyLIdLa16bfyvGozloJ0hwcumocdSClFNIIrWaA35D_bsJcsOLUfQx7CUQRlE/exec'
+export const ADMIN_CHANGE_FILE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxDxTmqmZagTWO0sGIvamaNHr7P5O3k4DDkNPURGdnpQ4GpqSPuunl2iJyB5Qe8Bx1E/exec'
 export const FEEDBACK_URL = 'https://forms.gle/6ffKJi8ydXiFfK7d6'
 export const OLD_DRIVE_URL = 'https://drive.google.com/drive/folders/1xlu4bteqX0QuIoBMBnnw3kz2Q2rk1rlI'
 export const GOOGLE_DRIVE_URL = 'https://drive.google.com/drive/folders/1se47Kt060R0j3zt7n9Bx-TsSfF34ko93'
